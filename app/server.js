26fea43b45989d979c3705dff4a7be649bec554a
LAPTOP-2GD2SQUR <gregory.saison@gmail.com>
2022-05-10 23:47:30 +0200

    fix server.listen et ajout logique de rendu routeur

--- app/server.js
+++ app/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const router = require("./router");
 
 const server = express();
 const port = process.env.PORT ?? 3000;
 
-server.listen((port) => {
+server.use(router);
+
+server.listen(port, () => {
   console.log(`Serveur lancé à l'adresse http://localhost:${port}`);
 });
