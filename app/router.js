74b3753db16de8ef3383b3e67fad17e802fa490a
LAPTOP-2GD2SQUR <gregory.saison@gmail.com>
2022-05-10 23:54:05 +0200

    refacto: mise en place d'un controleur dans la logique de rendu

--- app/router.js
+++ app/router.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const renderController = require("./controllers/renderController");
 
-router.get("/", (__, res) => {
-  res.send("Hello World");
-});
+router.get("/", renderController.renderHello);
 
 module.exports = router;
